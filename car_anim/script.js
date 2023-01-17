c8b52ac1893baff5d035724c799125fd1b20253e
ashi80ashi <ashi80ashi@gmail.com>
2023-01-17 15:32:38 +0530

    deleted car project

--- car_anim/script.js
+++
@@ -1,7 +0,0 @@
-let wheel = document.getElementById("wheel");
-
-
-console.log(wheel)
-wheel.addEventListener("click",()=>{
-    document.getElementById("carbody").style.animation="carotate 2s linear  ";
-})
