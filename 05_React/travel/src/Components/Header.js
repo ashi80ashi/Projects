62808fee925f6b543cc2751636a8979fa7e2eb88
ashi80ashi <ashi80ashi@gmail.com>
2023-03-19 20:30:59 +0530

    axios project

--- 05_React/travel/src/Components/Header.js
+++ 05_React/travel/src/Components/Header.js
@@ -1,6 +1,6 @@
 function Header() {
     return(
-        <h1 className="text-center p-8 font-bold text-3xl bg-slate-500  "><span className="bg-gray-200 w-3/12 p-1 hover:scale-110 rounded-md inline-block">Babbar Travelers &#x1F981;</span></h1>
+        <h1 className="text-center p-8 font-bold sm:text-xl md:text-2xl lg:text-3xl bg-slate-500  "><span className="bg-gray-200 w-3/12 p-1 hover:scale-110 rounded-md inline-block">Babbar Travelers &#x1F981;</span></h1>
     );
 }
 export default Header;
