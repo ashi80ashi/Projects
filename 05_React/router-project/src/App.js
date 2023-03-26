be50c5341a79686768b9b9fefd8eaaab24ec3388
ashi80ashi <ashi80ashi@gmail.com>
2023-03-26 22:32:16 +0530

    updated

--- 05_React/router-project/src/App.js
+++ 05_React/router-project/src/App.js
@@ -14,16 +14,18 @@ function App() {
 
   return (
     <div className="bg-neutral-700">
+      
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+     
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
         <Route path="/singup" element={<Signup setIsLoggedIn={setIsLoggedIn}/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-        
+        <Route path="/contact" element={<Contact/>} />  
       </Routes>
+
     </div>
   );
 }
