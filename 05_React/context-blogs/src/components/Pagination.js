99d92287d17911f08594c378b4810dd418912404
ashi80ashi <ashi80ashi@gmail.com>
2023-04-05 23:29:45 +0530

    updt

--- 05_React/context-blogs/src/components/Pagination.js
+++ 05_React/context-blogs/src/components/Pagination.js
@@ -6,14 +6,14 @@ function Pagination() {
   const {page,handelpagechang,totalPages} = useContext(AppContext)
   return (
     <div className='flex justify-between px-10 py-5 font-semibold text-lg shadow shadow-inner shadow-black'>
-      <div className=' flex justify-between w-1/12'>
+      <div className=' flex justify-between gap-5 '>
       {
         page > 1 &&
-        <button onClick={() => handelpagechang(page - 1)}>Previous</button>
+        <button className=' border-2 px-5 border-black' onClick={() => handelpagechang(page - 1)}>Previous</button>
       }
       {
         page < totalPages &&
-        <button onClick={() => handelpagechang(page + 1)}>Next</button>
+        <button className=' border-2 px-5   border-black' onClick={() => handelpagechang(page + 1)}>Next</button>
       }
     </div>
     <p>page {page} of {totalPages}</p>
