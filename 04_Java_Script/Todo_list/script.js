bd85c6e244fa3fc95d7f1d309dd30e228cc0d2b6
ashi80ashi <ashi80ashi@gmail.com>
2023-04-20 13:58:23 +0530

    formatted

--- 04_Java_Script/Todo_list/script.js
+++ 04_Java_Script/Todo_list/script.js
@@ -1,36 +1,34 @@
-
 let form = document.querySelector("form");
 
-let tasks=[];
+let tasks = [];
 
-function rend(){
-    
-    let html='';
-    tasks.forEach((value,index)=>{
-        console.log(index,value.description)
-        html+=`<div class="task" onClick='del(${index})' >
+function rend() {
+  let html = "";
+  tasks.forEach((value, index) => {
+    console.log(index, value.description);
+    html += `<div class="task" onClick='del(${index})' >
         <div>
             <p>${value.title}</p>
             <span>${value.description}</span>
         </div>
         <button>-</button>
         </div>`;
-    });
+  });
 
-    document.getElementById("taskholder").innerHTML=html;
+  document.getElementById("taskholder").innerHTML = html;
 }
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    let title = document.getElementById("title").value;
-    let description =document.getElementById("description").value;
-    tasks.push({title:title,description:description});
-    console.log(tasks)
-   // console.log(tasks);
-    rend();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+  tasks.push({ title: title, description: description });
+  console.log(tasks);
+  // console.log(tasks);
+  rend();
 });
 
-function del(index){
-    delete tasks[index];
-    rend();
+function del(index) {
+  delete tasks[index];
+  rend();
 }
